ceacb48dbdb8f3179cfac6d83dc714353fd65fc5
Darren O'Neale <doneale3@gmail.com>
2020-07-05 22:49:28 -0400

    adding nodejs api

---
+++ calculator-api/index.js
@@ -0,0 +1,65 @@
+const Calculator = require('./src/lambda/calculator.js');
+
+const validateParams = function(params) {
+    var ret = params;
+    if (ret.x == undefined || ret.y == undefined) {
+        return {
+            error: 'Unexpected params. Expected parameters x and y to be integers.'
+        }
+    }
+    ret.x = parseInt(ret.x);
+    ret.y = parseInt(ret.y);
+    return ret;
+}
+
+exports.handler = async (event) => {
+    console.log(event);
+    
+    var responseMessage = "";
+    var func = null;
+    switch (event.path) {
+        case "/calculator-api/add": func = Calculator.add; break;
+        case "/calculator-api/subtract": func = Calculator.subtract; break;
+        
+        default:
+            return {
+                statusCode: 502,
+                body: 'This path is unsupported by the API.'
+            }
+    }
+    
+    var params = null;
+    switch (event.httpMethod) {
+        case "GET": 
+            // write some logic
+            params = validateParams(event.queryStringParameters);
+            break;
+        
+        case "POST": 
+            // write some logic
+            params = validateParams(JSON.parse(event.body));
+            break;
+            
+        default: 
+            // no-op
+            responseMessage = "HTTP METHOD IS NOT SUPPORTED";
+            break;
+    }
+
+    if (params != null) {
+        if (params.error) {
+            responseMessage = params.error;
+        } else {
+            responseMessage = {
+                value: func(params.x, params.y)
+            }
+        }
+    }
+    
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(responseMessage),
+    };
+    return response;
+};
---
+++ calculator-api/src/lambda/calculator.js
@@ -0,0 +1,13 @@
+
+class Calculator
+{
+    add(x, y) {
+        return x + y;
+    }
+
+    subtract(x, y) {
+        return x - y;
+    }
+}
+
+module.exports = new Calculator();
